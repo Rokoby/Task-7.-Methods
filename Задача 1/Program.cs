a89b76a5eadb62d5fe53f6469d500edb02ad13cd
User <tpsshareduser@mail.ru>
2024-03-02 17:05:27 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите параметры");
+            int a1 = Convert.ToInt16(Console.ReadLine());
+            int b1 = Convert.ToInt16(Console.ReadLine());
+            int c1 = Convert.ToInt16(Console.ReadLine());
+            int a2 = Convert.ToInt16(Console.ReadLine());
+            int b2 = Convert.ToInt16(Console.ReadLine());
+            int c2 = Convert.ToInt16(Console.ReadLine());
+            double s1 = Square(a1, b1, c1);
+            double s2 = Square(a2, b2, c2);
+            if (s1 > s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+                if (s1 < s2)
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площади равын");
+            }
+            Console.ReadKey();
+        }
+        static double Square(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра");
+            int a = Convert.ToInt16(Console.ReadLine());
+            int s;
+            int v;
+            Calculation(a, out s, out v);
+            Console.WriteLine("s - {0}, v - {1}", s, v);
+            Console.ReadKey();
+        }
+        static void Calculation (int a, out int s, out int v)
+        {
+            s = a * a;
+            v = a * a * a;
+        }
+    }
+}
